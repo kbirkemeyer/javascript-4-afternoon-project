8d9e792551b498ae21c08607effe8b071c36d54d
Katie <adlerk8@gmail.com>
2020-10-14 15:23:19 -0700

    context portion ready for review; constructors in progress

--- constructors.js
+++ constructors.js
@@ -101,6 +101,21 @@ function Employee(name, email, hireDate) {
   // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
   
   // Code here
+  function User(name, age, email, savedPosts) {
+  this.name = name;
+  this.age = age;
+  this.email = email;
+  this.savedPosts = savedPosts;
+  }
+
+  User.prototype.addSavedPost = function(id, title, rating) {
+    this.id = id;
+    this.title = title;
+    this.rating = rating;
+    return this.savedPosts.push({id, title, rating});
+    }
+
+  
   
   ////////// PROBLEM 6 //////////
   
@@ -108,6 +123,13 @@ function Employee(name, email, hireDate) {
   // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
   
   // Code here
+  User.prototype.removeSavedPost = function(num) {
+  for(let i = 0; i <= this.savedPosts.length; i++){
+    if (this.savedPosts[i].id === num) {
+      this.savedPosts.splice(i, 1);
+    } 
+  }
+  }
   
   ////////// PROBLEM 7 //////////
   
@@ -115,4 +137,7 @@ function Employee(name, email, hireDate) {
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
   // Code here
-  
+  User.prototype.changePostRating = function(num1, num2) {
+  if (this.savedPosts.indexOf(num1) === num1)
+    this.savedPosts[num1].rating = num2;
+  }
